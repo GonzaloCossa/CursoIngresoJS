fe31c63d0aed86c9f4fc90709bd1211eea0ceb3a
Gonzalo <gonzalocossa67@gmail.com>
2022-01-28 12:35:25 -0300

    prueba

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Mami el Bocero Llegó");
+	alert(ingreso 2020);
 }
 
