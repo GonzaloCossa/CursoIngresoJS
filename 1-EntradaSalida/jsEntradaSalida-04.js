598467da7c10fe8dbd7cc054d71920a706460e16
Gonzalo <gonzalocossa67@gmail.com>
2022-01-28 13:57:31 -0300

    Ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -9,7 +9,7 @@ function mostrar()
 
  nombre = PROMPT("Ingrese su nombre");
 
- document.getElementsById('txtIdNombre').value = nombre; 
+ document.getElementById('txtIdNombre').value = nombre; 
 
 }
 
