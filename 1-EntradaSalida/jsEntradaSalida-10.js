98a1a36b7843ee1b19611707c88afe091d7d3e56
Gonzalo <gonzalocossa67@gmail.com>
2022-02-02 17:20:54 -0300

    push

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -2,8 +2,22 @@
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
-{
-	alert("ok");
-}
+en el cuadro de texto "RESULTADO"
+
+Gonzalo Cossa E/S Ejercicio 10
+Div B
+*/
+function mostrarAumento() {
+	var importe;
+	var descuento;
+	var resultado;
+
+	importe = document.getElementById("txtIdImporte").value;
+	resultado = document.getElementById("txtIdResultado").value;
+
+	descuento = 0.25;
+
+	importe = parseInt(importe);
+	resultado = importe - (importe * descuento);
+	document.getElementById("txtIdResultado").value = resultado;
+}	
