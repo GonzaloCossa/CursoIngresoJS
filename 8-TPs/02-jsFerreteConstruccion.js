7918a6d4d4fd376b8fe66f2fc8d8919e92a01cd3
Gonzalo <gonzalocossa67@gmail.com>
2022-02-03 12:22:24 -0300

    push

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -2,10 +2,11 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+
+Gonzalo Cossa TP N°2
+Div B
 */
-// txtIdLargo txtIdAncho txtIdRadio
-function Rectangulo () 
-{
+function Rectangulo() {
     var largoRectangulo;
     var anchoRectangulo;
     var alambreRectangulo;
@@ -14,7 +15,7 @@ function Rectangulo ()
     largoRectangulo = document.getElementById("txtIdLargo").value;
     anchoRectangulo = document.getElementById("txtIdAncho").value;
 
-    largoRectangulo = parseFloat(largoRectangulo);// cambiamos a parsefloat porque las medidas pueden ser numeros con coma
+    largoRectangulo = parseFloat(largoRectangulo);
     anchoRectangulo = parseFloat(anchoRectangulo);
 
     perimetroDelTerreno = (anchoRectangulo + largoRectangulo) * 2;
@@ -23,10 +24,9 @@ function Rectangulo ()
     alert("La cantidad de alambre a comprar es " + alambreRectangulo);
 }
 
-function Circulo () 
-{
+function Circulo() {
     var radioCirculo;
-    var perimetroCirculo;	
+    var perimetroCirculo;
     var vueltaAlambre;
     var cantidadAlambre;
     var mensaje;
@@ -45,31 +45,28 @@ function Circulo ()
     mensaje = "La cantidad total es " + cantidadAlambre.toFixed();
 
     alert(mensaje);
-
 }
 
-function Materiales () 
-{
-	var largoDelTerreno;
+function Materiales() {
+    var largoDelTerreno;
     var anchoDelTerreno;
     var areaDelTerreno;
     var bolsaDeCemento;
     var bolsaDeCal;
     var mensaje;
 
-    largoDelTerreno= document.getElementById("txtIdLargo").value;
-    anchoDelTerreno= document.getElementById("txtIdAncho").value;
+    largoDelTerreno = document.getElementById("txtIdLargo").value;
+    anchoDelTerreno = document.getElementById("txtIdAncho").value;
 
-    largoDelTerreno= parseFloat(largoDelTerreno);
-    anchoDelTerreno= parseFloat(anchoDelTerreno);
+    largoDelTerreno = parseFloat(largoDelTerreno);
+    anchoDelTerreno = parseFloat(anchoDelTerreno);
 
-    areaDelTerreno= largoDelTerreno * anchoDelTerreno;
-    bolsaDeCemento= areaDelTerreno * 2;
-    bolsaDeCal= areaDelTerreno * 3;
-     //console.log(bolsaDeCal, bolsaDeCemento);
+    areaDelTerreno = largoDelTerreno * anchoDelTerreno;
+    bolsaDeCemento = areaDelTerreno * 2;
+    bolsaDeCal = areaDelTerreno * 3;
 
-     mensaje = "la cantidad de bolsas de cemento a comprar son " + bolsaDeCemento; 
-     mensaje += " y la cantidad de bolsas de cal a comprar son " + bolsaDeCal;
+    mensaje = "la cantidad de bolsas de cemento a comprar son " + bolsaDeCemento;
+    mensaje += " y la cantidad de bolsas de cal a comprar son " + bolsaDeCal;
 
     alert(mensaje);
 }
