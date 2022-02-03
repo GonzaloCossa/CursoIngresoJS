458f18168289e45a40fdf653cd7897d8e8b0b024
Gonzalo <gonzalocossa67@gmail.com>
2022-02-03 11:32:16 -0300

    push

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -2,36 +2,75 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
-
-Gonzalo Cossa TP N°2
-
 */
-
-function Rectangulo() {
-    var largoDelTerreno;
-    var anchoDelTerreno;
-    var cantidadDeAlambre;
+// txtIdLargo txtIdAncho txtIdRadio
+function Rectangulo () 
+{
+    var largoRectangulo;
+    var anchoRectangulo;
+    var alambreRectangulo;
     var perimetroDelTerreno;
-    var vueltasDeAlambre;
 
-    largoDelTerreno = document.getElementById("txtIdLargo").value;
-    anchoDelTerreno = document.getElementById("txtIdAncho").value;
+    largoRectangulo = document.getElementById("txtIdLargo").value;
+    anchoRectangulo = document.getElementById("txtIdAncho").value;
 
-    largoDelTerreno = parseFloat(largoDelTerreno);
-    anchoDelTerreno = parseFloat(anchoDelTerreno);
+    largoRectangulo = parseFloat(largoRectangulo);// cambiamos a parsefloat porque las medidas pueden ser numeros con coma
+    anchoRectangulo = parseFloat(anchoRectangulo);
 
-    vueltasDeAlambre = 3
-    perimetroDelTerreno = (2 * largoDelTerreno) + (2 * anchoDelTerreno);
-    cantidadDeAlambre = perimetroDelTerreno * cantidadDeAlambre;
+    perimetroDelTerreno = (anchoRectangulo + largoRectangulo) * 2;
+    alambreRectangulo = perimetroDelTerreno * 3;
 
-    alert("La cantidad de alambre a comprar es " + cantidadDeAlambre);
+    alert("La cantidad de alambre a comprar es " + alambreRectangulo);
 }
-function Circulo() {
-    var vantidadDeAlambre
-    var radioDelTerreno
 
+function Circulo () 
+{
+    var radioCirculo;
+    var perimetroCirculo;	
+    var vueltaAlambre;
+    var cantidadAlambre;
+    var mensaje;
+
+    vueltaAlambre = 3
+    radioCirculo = document.getElementById("txtIdRadio").value;
+
+    radioCirculo = parseFloat(radioCirculo);
+
+    perimetroCirculo = 2 * Math.PI * radioCirculo;
+
+    //console.log(Math.PI);
+
+    cantidadAlambre = perimetroCirculo * vueltaAlambre;
+
+    mensaje = "La cantidad total es " + cantidadAlambre.toFixed();
+
+    alert(mensaje);
 
 }
-function Materiales() {
 
+function Materiales () 
+{
+	var largoDelTerreno;
+    var anchoDelTerreno;
+    var areaDelTerreno;
+    var bolsaDeCemento;
+    var bolsaDeCal;
+    var mensaje;
+
+    largoDelTerreno= document.getElementById("txtIdLargo").value;
+    anchoDelTerreno= document.getElementById("txtIdAncho").value;
+
+    largoDelTerreno= parseFloat(largoDelTerreno);
+    anchoDelTerreno= parseFloat(anchoDelTerreno);
+
+    areaDelTerreno= largoDelTerreno * anchoDelTerreno;
+    bolsaDeCemento= areaDelTerreno * 2;
+    bolsaDeCal= areaDelTerreno * 3;
+     //console.log(bolsaDeCal, bolsaDeCemento);
+
+     mensaje = "la cantidad de bolsas de cemento a comprar son " + bolsaDeCemento; 
+     mensaje += " y la cantidad de bolsas de cal a comprar son " + bolsaDeCal;
+
+    alert(mensaje);
 }
+
