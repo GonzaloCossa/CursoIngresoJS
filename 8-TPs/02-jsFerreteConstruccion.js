b6d031ef8767466e5c74c96f6cf542d4025b9911
Gonzalo <gonzalocossa67@gmail.com>
2022-02-03 09:51:57 -0300

    push

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -2,17 +2,36 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+
+Gonzalo Cossa TP N°2
+
 */
 
-function Rectangulo () 
-{
+function Rectangulo() {
+    var largoDelTerreno;
+    var anchoDelTerreno;
+    var cantidadDeAlambre;
+    var perimetroDelTerreno;
+    var vueltasDeAlambre;
+
+    largoDelTerreno = document.getElementById("txtIdLargo").value;
+    anchoDelTerreno = document.getElementById("txtIdAncho").value;
+
+    largoDelTerreno = parseFloat(largoDelTerreno);
+    anchoDelTerreno = parseFloat(anchoDelTerreno);
+
+    vueltasDeAlambre = 3
+    perimetroDelTerreno = (2 * largoDelTerreno) + (2 * anchoDelTerreno);
+    cantidadDeAlambre = perimetroDelTerreno * cantidadDeAlambre;
+
+    alert("La cantidad de alambre a comprar es " + cantidadDeAlambre);
+}
+function Circulo() {
+    var vantidadDeAlambre
+    var radioDelTerreno
+
 
 }
-function Circulo () 
-{
-	
-}
-function Materiales () 
-{
-	
+function Materiales() {
+
 }
