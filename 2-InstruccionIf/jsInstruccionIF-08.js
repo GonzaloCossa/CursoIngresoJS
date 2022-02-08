89a040b920ed5205ab0f89887b4bcdaa09c688ba
Gonzalo <gonzalocossa67@gmail.com>
2022-02-08 11:47:47 -0300

    push

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -12,8 +12,9 @@ function mostrar()
 	edad = document.getElementById("txtIdEdad").value;
 	estadoCivil = document.getElementById("estadoCivil").value;
 
-	if(!(edad < 18) && (estadoCivil = "Soltero")){
-		alert("Es soltero y no es menor.'");
+	if(edad > 17  &&  estadoCivil == "Soltero")
+	{
+		alert("Es soltero y no es menor")
 	}
 	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,3 +1,9 @@
+/*
+
+Alumno: Gonzalo Agustín Cossa
+Div B
+Instrucción 'IF' Ejercicio N°9
+*/
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
