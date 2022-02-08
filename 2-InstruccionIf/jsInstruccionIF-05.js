580ceb63e5643c366ee29a80a083b0c21f6f8cf9
Gonzalo <gonzalocossa67@gmail.com>
2022-02-08 11:03:46 -0300

    push

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,18 @@
+/*
+
+Alumno: Gonzalo Agustín Cossa
+Div B
+Instrucción 'IF' Ejercicio N°5
+*/
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad;
+
+	edad = document.getElementById("txtIdEdad").value;
+
+	if(!(edad < 18 && edad >12)){
+	
+		alert("La persona no es adolescente.");
+	}
 
 }//FIN DE LA FUNCIÓN
