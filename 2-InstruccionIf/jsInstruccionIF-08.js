4b778daa2495a9457665bb5514463b63790e5ab3
Gonzalo <gonzalocossa67@gmail.com>
2022-02-08 11:32:00 -0300

    push

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -12,9 +12,8 @@ function mostrar()
 	edad = document.getElementById("txtIdEdad").value;
 	estadoCivil = document.getElementById("estadoCivil").value;
 
-	if(edad < 18 && estadoCivil != "Soltero"){
+	if(!(edad < 18) && (estadoCivil = "Soltero")){
+		alert("Es soltero y no es menor.'");
 	}
-	else(edad > 17) && (estadoCivil = "Soltero")
-		alert("Es soltero y no es menor.");}
 	
 }//FIN DE LA FUNCIÓN
