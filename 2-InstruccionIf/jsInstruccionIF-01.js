c9c0ee5b0ad4875cf8c0bc3f158b88839c2740a1
Gonzalo <gonzalocossa67@gmail.com>
2022-02-08 09:26:16 -0300

    push

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,8 +1,6 @@
 function mostrar() {
 	var edad = document.getElementById("txtIdEdad").value;
 	var comparacion = 15;
-
-	edad = parseInt(edad);
 	var resultado = edad == comparacion;
 
 	if (resultado) {
