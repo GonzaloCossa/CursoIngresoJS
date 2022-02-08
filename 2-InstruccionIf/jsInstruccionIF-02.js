812019284a895caeed47e1e9cb3633bed5ccdba6
Gonzalo <gonzalocossa67@gmail.com>
2022-02-08 09:25:22 -0300

    push

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,15 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
-	
+function mostrar() {
+	var edad;
+	var mayorDeEdad;
+	var resultado;
 
+	edad = document.getElementById("txtIdEdad").value;
+	mayorDeEdad = 18;
+
+	resultado = (edad >= 18) && (mayorDeEdad >= 18);
+
+	if (resultado) {
+
+		alert(edad + " es mayor de edad");
+	}
 }//FIN DE LA FUNCIÓN
