2f3ff4bcf56bed0dea814bad0b5e21fbaab85116
Gonzalo <gonzalocossa67@gmail.com>
2022-02-08 10:25:36 -0300

    push

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,15 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+function mostrar() {
+	var edadInicial;
+	var resultado;
 
+	edadInicial = document.getElementById("txtIdEdad").value;
+	edadInicial = parseInt(edadInicial);
+	
+	resultado = (edadInicial > 12) && (edadInicial < 18);
 
+	if (resultado) 
+	{
+		alert(edadInicial + " usted es adolecente");
+	}
+	
 }//FIN DE LA FUNCIÓN
