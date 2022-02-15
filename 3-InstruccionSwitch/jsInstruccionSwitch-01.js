37c53c25cb20fc1525d3e8c6ce9b3f0dd2a52b6a
Gonzalo <gonzalocossa67@gmail.com>
2022-02-15 09:24:27 -0300

    push

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,31 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mesDelAño;
 
+	mesDelAño = document.getElementById("txtIdMes").value;
 
+	switch(mesDelAño)
+	{
+		case "Enero":
+			alert("Es " + mesDelAño + " que comiences bien el año!!!." );
+		break;
 
+		case "Marzo":
+			alert("Es " + mesDelAño + " a clases!!!." );
+		break;
+		
+		case "Julio":
+			alert("Es " + mesDelAño + " se vienen las vacaciones!!!.");
+		break;
+		
+		case "Diciembre":
+			alert("Es " + mesDelAño + " felices fiesta!!!." );
+		break;
+		
+		default:
+			alert("No pasa nada");
+		break;
+		
+	}
 }//FIN DE LA FUNCIÓN
