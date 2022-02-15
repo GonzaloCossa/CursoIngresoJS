e077667c457b42e78a37f20a22322bd80da73e7b
Gonzalo <gonzalocossa67@gmail.com>
2022-02-15 09:53:11 -0300

    push

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,29 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
 
+	var mesDelAño;
 
+	mesDelAño = document.getElementById("txtIdMes").value;
 
-
+	switch(mesDelAño)
+	{
+	case "Julio":
+	case "Agosto":
+		alert("Es " + mesDelAño + " Abrigate que hace frio." );
+		break;
+	
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+		alert("Es " + mesDelAño + " Falta para el invierno." );
+		break;
+		
+	default:
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+	}
 }//FIN DE LA FUNCIÓN
