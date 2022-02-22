2417b5fc534517f4cf24cee6d6e6aab89c8df521
Gonzalo <gonzalocossa67@gmail.com>
2022-02-22 09:44:53 -0300

    push

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,19 +3,42 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
+	var bandera;
 	var numeroIngresado;
 	var numeroMaximo;
 	var numeroMinimo;
 	var respuesta;
+
 	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+	respuesta = "si";
+	bandera = "Es el primer número.";
+
+	while(respuesta == "si")
 	{
-		
-		respuesta=prompt("desea continuar?");
+		numeroIngresado = prompt("Ingrese un número.");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		if(bandera == "Es el primer número.")
+		{
+			bandera = "No es el primer número.";
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+		}
+
+		else
+		{
+			if(numeroIngresado > numeroMaximo)
+			{
+				numeroMaximo = numeroIngresado;
+			}
+			else
+				if (numeroIngresado < numeroMinimo)
+				{	
+					numeroMinimo = numeroIngresado;
+				}
+		}
+		respuesta = prompt("¿Quiere seguir ingresando números?","si");
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	document.getElementById("txtIdMaximo").value = numeroMaximo;
+	document.getElementById("txtIdMinimo").value = numeroMinimo;
 }//FIN DE LA FUNCIÓN
