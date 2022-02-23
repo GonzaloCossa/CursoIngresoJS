79929b0a775e3c9600a456fead7f5a52db90b58e
Gonzalo <gonzalocossa67@gmail.com>
2022-02-23 08:44:42 -0300

    push

--- 6-InstruccionFor/jsIteracionesFor-03.js
+++ 6-InstruccionFor/jsIteracionesFor-03.js
@@ -1,8 +1,11 @@
 function mostrar()
 {
-
-	var repetciones = prompt("ingrese el número de repeticiones");
-	alert("ok");
-
-
+	var repeticiones;
+	
+	repeticiones = prompt("ingrese el número de repeticiones");
+	
+	for(repeticiones = repeticiones; repeticiones > 0 ; repeticiones --)
+	{
+		document.write("Hola UTN FRA" + "<br>");
+	}
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor-04.js
+++ 6-InstruccionFor/jsIteracionesFor-04.js
@@ -1,7 +1,14 @@
 function mostrar()
 {
-	alert("ok");
-
-
-
+	var repeticiones;
+	
+	for(repeticiones = 0 ; ; repeticiones ++)
+	{
+		console.log(repeticiones);
+		if(repeticiones == 100)
+		{
+			document.write("Hola UTN FRA" + "<br>");
+			break;
+		}
+	}
 }//FIN DE LA FUNCIÓN
