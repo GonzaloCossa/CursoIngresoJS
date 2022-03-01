02495777448502df8c25f9fab95ab228016c592d
Gonzalo <gonzalocossa67@gmail.com>
2022-02-28 21:44:03 -0300

    push

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -17,21 +17,37 @@ function mostrar()
 	var sexo;
 	var nota;
 	var nombre;
+
 	var contadorMujeres;
 	var contadorHombres;
 	var contadorNb;
+	var contadorDesaprobados;
+	var contadorTotal;
 	var totalEdadMujeres;
 	var totalEdadHombres;
 	var totalEdadNb;
 	var promedioMujeres;
 	var promedioHombres;
 	var promedioNb;
+	var porcentajeDesaprobados;
 	var genteConFiebre;
 	var genteSinFiebre;
-	var primerAprobado;
-	var notaAprobado;
-	var nombrePrimero;
-	var sexoPrimero;
+	
+	var banderaPrimerAprobado;
+	var primerAprobadoNombre;
+	var banderaTemparaturaMax;
+	var temperaturaMax;
+	var edadTemperaturaMax;
+	var nombreTemperaturaMax;
+	var nbMasJoven;
+	var banderaNbMasJoven;
+	var notaNbMasJoven;
+	var alturaNbMasJoven;
+	var mujerMasAlta;
+	var banderaMujerMasAlta;
+	var nombreMujerMasAlta;
+	var primerEnanoAprobado;
+	var nombrePetisoAprobado;
 	var respuesta;
 
 	genteConFiebre = 0;
@@ -39,15 +55,17 @@ function mostrar()
 	contadorMujeres = 0;
 	contadorHombres = 0;
 	contadorNb = 0;
-	promedioMujeres = 0;
-	promedioHombres = 0;
-	promedioNb = 0;
+	contadorDesaprobados = 0;
+	contadorTotal = 0
 	totalEdadMujeres = 0;
 	totalEdadHombres = 0;
 	totalEdadNb = 0;
-	notaAprobado = 0;
 	respuesta = "si";
-	alguienAprobo = true;
+	banderaPrimerAprobado = false;
+	banderaTemparaturaMax = false;
+	banderaMujerMasAlta = false;
+	banderaNbMasJoven = false;
+	primerEnanoAprobado = false;
 
 	while(respuesta == "si")
 	{
@@ -85,72 +103,152 @@ function mostrar()
 			}
 		
 		sexo = prompt("Ingrese su sexo. (f/m/nb)");
-			while(sexo != "f" && sexo != "m" && sexo != "nb" && !sexo)
+		while(sexo != "f" && sexo != "m" && sexo != "nb" && !sexo)
 			{
 				sexo = prompt("Ingrese un sexo valido.");
 			}
 
-			if(sexo == "f")
-			{
-				contadorMujeres ++; 
+		switch(sexo)
+		{
+			case "f": 
 				totalEdadMujeres += edad;
-			}
-			else
-			{
-				if(sexo == "m")
+				contadorMujeres ++;
+				
+			break;
+			case "m":
+				contadorHombres ++;
+				totalEdadHombres += edad;
+			break;
+
+			case "nb":
+				contadorNb ++;
+				totalEdadNb += edad;
+				
+				if(edad < nbMasJoven || banderaNbMasJoven == false)
 				{
-					contadorHombres ++;
-					totalEdadHombres += edad;
+				nbMasJoven = edad;
+				notaNbMasJoven = nota;
+				alturaNbMasJoven = altura;
+				banderaNbMasJoven = true;
 				}
-				else
-				{
-					if(sexo == "nb")
-					{
-						contadorNb ++;
-						totalEdadNb += edad;
-					}
-				}
-			}
+			break;
+
+			default:
+				sexo = prompt("Ingrese un sexo valido.");
+			break;
+		}
 
 		nota = prompt("Ingrese su nota (0-10).");
+		nota = parseInt(nota);
+		
 			while(nota > 10 || nota < 0 || !nota)
 			{
 				nota = prompt("Ingrese una nota valida.");
+				nota = parseInt(nota);
 			}
 
-		nombre = prompt("Ingrese su nombre.");	
+		nombre = prompt("Ingrese su nombre.");
+
+		// Primer aprobado
+		if(banderaPrimerAprobado == false && nota > 3) 
+		{
+		primerAprobadoNombre = nombre;
+		banderaPrimerAprobado = true; 
+		}
+
+		if(banderaPrimerAprobado == false)
+		{
+			primerAprobadoNombre = "No hay ningún aprobado."
+		}
+		
+		// Temperatura más alta
+		if(banderaTemparaturaMax == false || temperatura > temperaturaMax)
+		{
+			temperaturaMax = temperatura;
+			nombreTemperaturaMax = nombre;
+			edadTemperaturaMax = edad;
+			banderaTemparaturaMax = true;
+		}
+		
+		// Mujer aprobada más alta
+		if(sexo == "f" && nota > 3 && (banderaMujerMasAlta == false || altura > mujerMasAlta))
+		{
+			mujerMasAlta = altura;
+			nombreMujerMasAlta = nombre;
+			banderaMujerMasAlta = true;
+		}
+
+		if(sexo == "m" && nota < 4 && altura < 160 && primerEnanoAprobado == false)
+		{
+			nombrePetisoAprobado = nombre;
+			primerEnanoAprobado = true;
+		}
+		
+		if(banderaMujerMasAlta == false)
+			{
+				mujerMasAlta = "no hay mujer más alta";
+				nombreMujerMasAlta = "no hay mujer más alta";
+			}
+		
+		if(nota < 4)
+		{
+			contadorDesaprobados ++;
+		}
+		
+		contadorTotal ++;
 			
 		respuesta = prompt("¿Quiere seguir ingresando números?","si");
 	}	
-		if(nota >= 4)
+
+	if(contadorMujeres > 0)
 		{
-			if(alguienAprobo == true)
-			{
-				alguienAprobo = false;
-				notaAprobado = nota;
-				nombrePrimero = nombre;
-				sexoPrimero = sexo;
-			}
+		promedioMujeres = (totalEdadMujeres / contadorMujeres);
+		}
+	else
+	{
+		promedioMujeres = "No hay ninguna mujer";
+		nombreMujerMasAlta = "No hay ninguna mujer";
+	}
 
-			else
-			{
-				contadorDesaprobados ++;
-			}
-		}	
+	if(contadorHombres > 0)
+		{
+		promedioHombres = (totalEdadHombres / contadorHombres);
+		}
+	else
+	{
+		promedioHombres = "No hay ningún hombre";
+	}
 	
-	promedioMujeres = (totalEdadMujeres / contadorMujeres).toFixed(2);
-	promedioHombres = (totalEdadHombres / contadorHombres).toFixed(2);
-	promedioNb = (totalEdadNb / contadorNb).toFixed(2);
+	if(contadorNb > 0)
+		{
+		promedioNb = (totalEdadNb / contadorNb);
+		}
+	else
+	{
+		promedioNb = "No hay nadie no binario";
+		alturaNbMasJoven = 0;
+		notaNbMasJoven = 0;
+	}
 
-	promedioMujeres = parseFloat(promedioMujeres);
-	promedioHombres = parseFloat(promedioHombres);
-	promedioNb = parseFloat(promedioNb);
+	porcentajeDesaprobados = contadorDesaprobados * 100 / contadorTotal;
+	porcentajeDesaprobados = parseInt(porcentajeDesaprobados);
+
 	//a)
 		document.write("La cantidad de mujeres es: " + contadorMujeres + ". <br>");
 	//b)
 		document.write("La cantidad de alumos con fiebre es: " + genteConFiebre + " y la cantidad de la que no es: " + genteSinFiebre + ". <br>");
 	//c)
-		document.write("El promedio de edad por sexo es: F: " + promedioMujeres + ", M: " + promedioHombres + ", NB: " + promedioNb +". <br>");
+		document.write("El promedio de edad por sexo es: F: " + promedioMujeres + ", M: " + promedioHombres + ", NB: " + promedioNb + ". <br>");
 	//d)
-		document.write("El primero aprobado es: " + nombrePrimero + " su nota es: " + notaAprobado + " y su sexo es: " + sexoPrimero + ". <br>");
-}//FIN DE LA FUNCIÓN
+		document.write("El primero aprobado es: " + primerAprobadoNombre + ". <br>");
+	//e)
+		document.write("El alumno con la temperatura más alta registrada es: " + nombreTemperaturaMax + " , su edad es " + edadTemperaturaMax + ". <br>");
+	//f)
+		document.write("La altura de la persona no binaria más joven es: " + alturaNbMasJoven + "cm , su nota es " + notaNbMasJoven + ". <br>");
+	//g)
+		document.write("El nombre de la mujer más alta aprobada es: " + nombreMujerMasAlta + ". <br>");
+	//h)
+		document.write("El porcentaje de desaprobados es: " + porcentajeDesaprobados + "%. <br>");
+	//i)
+		document.write("El primer hombre menor a 160cm desaprobado es: " + nombrePetisoAprobado + ". <br>");
+	}//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,72 @@
+/* Bienvenidos.
+Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre 
+-100 y 100 (validar) hasta que el usuario quiera e informar al terminar 
+el ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo. */
 function mostrar()
 {
+    var letraIngresada;
+    var numeroIngresado;
+    var numerosPares = 0;
+    var numerosImpares = 0;
+    var contadorCeros = 0;
+    var sumaPositivos = 0;
+    var promedioPositivos = 0;
+    var cantidadPositivos = 0;
+    var sumaNegativos = 0;
+    var respuesta = "si";
+    var bandera = false;
 
+    while(respuesta == "si")
+        {
+            letraIngresada = prompt("Ingrese una letra.");
+
+            numeroIngresado = prompt("Ingrese un número.");
+            numeroIngresado = parseInt(numeroIngresado);
+
+            while(numeroIngresado < -100 || numeroIngresado > 100 || numeroIngresado == 0)
+            {
+                numeroIngresado = prompt("Ingrese un numero valido.");
+            }
+
+            if(numeroIngresado % 2 == 0)
+            {
+                numerosPares ++;
+            }
+            else
+            {
+                numerosImpares ++;
+            }
+
+            if(numeroIngresado == 0)
+            {
+                contadorCeros ++;
+            }
+
+            if(numeroIngresado > 0)
+            {
+                sumaPositivos = sumaPositivos + numeroIngresado;
+                cantidadPositivos ++;
+            }
+
+            else
+            {
+                sumaNegativos = sumaNegativos + numeroIngresado;
+            }
+
+            respuesta = prompt("¿Quiere seguir ingresando datos?","si");
+        }  
+        
+        promedioPositivos = sumaPositivos / cantidadPositivos;
+
+    document.write("La cantidad de números pares es: " + numerosPares + "<br>");
+    document.write("La cantidad de números impares es: " + numerosImpares + "<br>");
+    document.write("La cantidad de ceros es: " + contadorCeros + "<br>");
+    document.write("El promedio de los números positivos es: " + promedioPositivos + "<br>");
+    document.write("La suma de números negativos es: " + sumaNegativos + "<br>");
 }
